3bedb876e48ed2a99a3b356229a20f6d29e7d755
Vid <vid_github@zooid.org>
2015-05-18 20:49:37 -0400

    submit from command line

--- trace-raw.js
+++ trace-raw.js
@@ -1,15 +1,38 @@
-/* jslint node: true */
+/* jslint node: true, esnext: true */
 
 "use strict";
 
 var dns = require('dns'), net = require('net'), _ = require('lodash');
 var processor = require('./lib/processor.js');
 
-GLOBAL.debug = true;
-GLOBAL.sim = true;
+var options = {
+  include_platform_traceroute: false,
+  queries: 4,
+  timeout: 1000,
+  postal_code: "m1m",
+  maxhops: 30
+};
 
-var dest, dest_ip, options = {timeout: 1000, queries: 4, max_hops: 30};
+var p = process.argv.slice(2), dest = 'ixmaps.ca';
 
-dest = process.argv[2] || 'ixmaps.ca';
+for (let i = 0; i < p.length; i++) {
+  let a = p[i];
+  if (a.indexOf('-') === 0) {
+    if (a === '-p') {
+      options.include_platform_traceroute = true;
+    } else {
+      console.error('usage:', process.argv[1], '[-p (include platform trace)]', 'dest');
+      process.exit();
+    }
+  } else {
+    dest = a;
+  }
+}
 
-processor.runTrace(dest, options);
+options.dest = dest;
+
+console.log(options);
+
+processor.doTrace(options, function(type, message) {
+  console.log(type, message);
+});
