2342169254a8f302ad7fd1ab75408669dfc78cd0
Vid <vid_github@zooid.org>
2015-06-24 20:53:41 -0400

    add max sequential errors, error messages, unresponsive timeout

--- src/js/InputForm.jsx
+++ src/js/InputForm.jsx
@@ -13,6 +13,7 @@ var defaultOptions = {
   timeout: 750,
   maxhops: 24,
   raw_protocol: 'ICMP',
+  max_sequential_errors: 3,
   include_platform_traceroute: false,
   platform_protocol: 'Default',
   platform_limit_ms: 60000
--- src/js/Options.jsx
+++ src/js/Options.jsx
@@ -13,24 +13,34 @@ module.exports = React.createClass({
       <div>
         <h1>Options</h1>
         <Panel>
-          <Input ref='queries' type='text' label='Queries' defaultValue={this.props.options.queries} labelClassName='col-md-4' wrapperClassName='col-md-1' />
-          <Input ref='timeout' type='text' label='Timeout (milliseconds)' defaultValue={this.props.options.timeout} labelClassName='col-md-4' wrapperClassName='col-md-2' />
-          <Input ref='maxhops' type='text' label='Maximum hops' defaultValue={this.props.options.maxhops} labelClassName='col-md-4' wrapperClassName='col-md-1' />
-          <Input type="select" defaultValue={this.props.options.raw_protocol} ref="raw_protocol" label='Raw protocol' labelClassName='col-md-4' wrapperClassName='col-md-2'>
+          <Input ref='queries' type='text' label='Queries' help="The number of sets of hops" defaultValue={this.props.options.queries}
+            style={{width: '3em'}} labelClassName='col-md-4' wrapperClassName='col-md-8' />
+          <Input ref='timeout' type='text' label='Timeout (milliseconds)' help="Maximum time to live of a hop probe" defaultValue={this.props.options.timeout}
+            style={{width: '5em'}} labelClassName='col-md-4' wrapperClassName='col-md-8' />
+          <Input ref='maxhops' type='text' label='Maximum hops' help="Limit of router hops" defaultValue={this.props.options.maxhops}
+            style={{width: '4em'}} labelClassName='col-md-4' wrapperClassName='col-md-8' />
+          <Input ref='max_sequential_errors' type='text' help="Number of timeouts or errors in a row before stopping query"
+            style={{width: '4em'}} label='Maximum sequential errors' defaultValue={this.props.options.max_sequential_errors} labelClassName='col-md-4' wrapperClassName='col-md-8' />
+          <Input type="select" defaultValue={this.props.options.raw_protocol} ref="raw_protocol" label='Raw protocol'
+            labelClassName='col-md-4' wrapperClassName='col-md-2'>
             {protocols}
           </Input>
         </Panel>
         <Panel>
-          <Input ref="include_platform_traceroute" type='checkbox' defaultChecked={this.props.options.include_platform_traceroute} label='Include platform traceroute' wrapperClassName='col-md-offset-2 col-md-10' />
-          <Input type="select" defaultValue={this.props.options.platform_protocol} ref="platform_protocol" label='Platform protocol' labelClassName='col-md-4' wrapperClassName='col-md-2'>
+          <Input ref="include_platform_traceroute" type='checkbox' help="Include output of the platform's native traceroute program" defaultChecked={this.props.options.include_platform_traceroute} label='Include platform traceroute'
+            wrapperClassName='col-md-offset-2 col-md-8' />
+          <Input type="select" defaultValue={this.props.options.platform_protocol} ref="platform_protocol" label='Platform protocol'
+            labelClassName='col-md-4' wrapperClassName='col-md-2'>
             <option>Default</option>
             {protocols}
           </Input>
-          <Input ref='platform_limit_ms' type='text' label='Platform overall timeout (seconds)' defaultValue={this.props.options.platform_limit_ms / 1000} labelClassName='col-md-4' wrapperClassName='col-md-1' />
+          <Input ref='platform_limit_ms' type='text' label='Platform overall timeout (seconds)' defaultValue={this.props.options.platform_limit_ms / 1000}
+            labelClassName='col-md-4' wrapperClassName='col-md-1' />
         </Panel>
         <Button className="pull-right" onClick={this.use}>Use these settings</Button>
         <Button className="pull-right" onClick={this.defaults}>Use defaults</Button>
         <Button className="pull-right" onClick={this.cancel}>Cancel</Button>
+        <br />
       </div>
     );
   },
@@ -38,7 +48,7 @@ module.exports = React.createClass({
     let options = {
       include_platform_traceroute: this.refs.include_platform_traceroute.getChecked()
     };
-    ['queries', 'timeout', 'maxhops', 'raw_protocol', 'platform_protocol', 'platform_limit_ms'].forEach(i => {
+    ['queries', 'timeout', 'maxhops', 'raw_protocol', 'max_sequential_errors', 'platform_protocol', 'platform_limit_ms'].forEach(i => {
       options[i] = this.refs[i].getValue();
     });
     options.platform_limit_ms = options.platform_limit_ms * 1000;
