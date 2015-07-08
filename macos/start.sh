b26e68add5ec20e51430db4e039c4256c65fd176
Vid <vid_github@zooid.org>
2015-07-07 22:45:52 -0400

    macos platypus

---
+++ macos/start.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash 
+
+echo
+echo "IXnode local server"
+echo
+echo "This minimal server requires root access to initiate low-level socket traces. We invite you to inspect the code for safety."
+echo 
+echo "A browser window for your regular user should appear. If the browser window doesn't open, please go to http://localhost:2040"
+echo
+echo "If you don't see the message "IXnode server started" something has gone wrong. Please consult the http://www.ixmaps.ca web site."
+echo
+
+DIR=`dirname "$0"`
+cd $DIR/../Resources/ixmaps-darwin
+
+echo "UID $UID DIR $DIR USER $USER"
+
+./node server.js
+
--- make_distributions.sh
+++ make_distributions.sh
@@ -2,19 +2,23 @@
 
 # make distributions for IXnode
 
-MODS="async cheerio express open raw-socket2 request socket.io"
+MODS="lodash open async cheerio express open raw-socket2 request socket.io"
 BASE="start.sh lib README.md server.js trace-platform.js trace-raw.js web"
 
+
 if [ ! -d dist ]; then mkdir dist; fi
-for i in linux darwin win32 win64 ; do
+for i in linux ixmaps-darwin win32 win64 ; do
   if [ ! -d dist/$i/node_modules ]; then mkdir -p dist/$i/node_modules; fi
   for m in $MODS; do
-    cp -a node_modules/$m dist/$i/node_modules/; 
+    rsync -a --delete-after node_modules/$m dist/$i/node_modules/; 
   done
+  # socket.io weirdness
+  if [ ! -d dist/$i/node_modules/socket.io/node_modules/socket.io-client ]; then rsync -a node_modules/socket.io-client dist/$i/node_modules/socket.io/node_modules/socket.io-client; fi
   for f in $BASE; do
-    cp -a $f dist/$i/
+    rsync -a --delete-after $f dist/$i/
   done 
 done
 
-
+# MacOS
+cp macos/start.sh dist/ixmaps-darwin/
 
