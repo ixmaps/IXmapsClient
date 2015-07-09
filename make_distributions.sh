053e1d23bd8b27af34b97353437d4df89e6a2cbe
Vid <vid_github@zooid.org>
2015-07-08 20:01:58 -0400

    fix path

--- make_distributions.sh
+++ make_distributions.sh
@@ -13,7 +13,7 @@ for i in linux ixmaps-darwin win32 win64 ; do
     rsync -a --delete-after node_modules/$m dist/$i/node_modules/; 
   done
   # socket.io weirdness
-  if [ ! -d dist/$i/node_modules/socket.io/node_modules/socket.io-client ]; then rsync -a node_modules/socket.io-client dist/$i/node_modules/socket.io/node_modules/socket.io-client; fi
+  if [ ! -d dist/$i/node_modules/socket.io/node_modules/socket.io-client ]; then rsync -a node_modules/socket.io-client dist/$i/node_modules/socket.io/node_modules/; fi
   for f in $BASE; do
     rsync -a --delete-after $f dist/$i/
   done 
