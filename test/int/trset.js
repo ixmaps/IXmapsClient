ca0ca11b7b9f2d922f51b317e71edc812b79f9d2
Vid <vid_github@zooid.org>
2015-07-12 17:14:34 -0400

    add testing trset

--- test/int/trset.js
+++ test/int/trset.js
@@ -10,12 +10,21 @@ test('retrieve trsets', function(t) {
   trset.getTrsets(function(err, trsets) {
     t.equals(null, err, 'no error');
     t.equals(true, trsets.length > 0, 'retrieve trsets');
+    t.equals(true, trsets.indexOf('testing') > 0, 'has testing trset');
     t.end();
     testset = trsets[0];
   });
 });
 
-test('retrieve trsets', function(t) {
+test('retrieve testing trset', function(t) {
+  trset.getTrset('testing', function(err, items) {
+    t.equals(null, err, 'no error');
+    t.equals(true, items.length > 0, 'retrieve trset');
+    t.end();
+  });
+});
+
+test('retrieve remote trset', function(t) {
   trset.getTrset(testset, function(err, items) {
     t.equals(null, err, 'no error');
     t.equals(true, items.length > 0, 'retrieve trset');
